7b7351464331bb2e3c911b234f9a9581fe2e0212
Dzmitry Tsybulski <dzmitrytsybulski@gmail.com>
2023-04-22 17:12:37 +0300

    Create Stage 1 Map Interface Tasks

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,20 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> outPutMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet()) {
+            if (outPutMap.get(entry.getValue()) != null) {
+                if (entry.getKey() < outPutMap.get(entry.getValue())) {
+                    outPutMap.put(entry.getValue(), entry.getKey());
+                }
+            } else {
+                outPutMap.put(entry.getValue(), entry.getKey());
+            }
+        }
+        return outPutMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> map = new HashMap<>();
+        StringTokenizer st1 = new StringTokenizer(sentence, " ,.");
+        while (st1.hasMoreTokens()) {
+            String value = st1.nextToken().toLowerCase(Locale.ROOT);
+            if (map.containsKey(value)) {
+                map.put(value, map.get(value) + 1);
+            } else {
+                map.put(value, 1);
+            }
+        }
+        return map;
     }
 }
